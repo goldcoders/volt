aa14155efe525478ca021497351d0dce7b87ccbb
Goldcoders <goldcoders@protonmail.com>
2020-09-21 17:40:03 +0800

    autofix

--- src/App.svelte
+++ src/App.svelte
@@ -28,15 +28,13 @@
   class="px-4 mx-auto mt-10 max-w-screen-xl sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
   <div class="text-center">
     <h2
-      class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900
-        sm:text-5xl sm:leading-none md:text-6xl">
+        class="text-4xl font-extrabold tracking-tight text-gray-900 leading-10 sm:text-5xl sm:leading-none md:text-6xl">
       {introduction}
       <br class="xl:hidden" />
-      <span class="text-indigo-600">{app.title}</span>
+      <span class="text-red-600">{app.title}</span>
     </h2>
     <div
-      class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5
-        md:text-xl md:max-w-3xl">
+        class="max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
       <h1 class="p-4 m-4 border rounded border-current">
         <span class="intro">Elegant UI Using TailwindCSS</span>
       </h1>

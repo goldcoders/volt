0f36ed9298c03c3868faf047d5441d2d8098e35e
Goldcoders <goldcoders@protonmail.com>
2020-09-21 18:10:53 +0800

    added tailwind styling

--- src/App.svelte
+++ src/App.svelte
@@ -13,7 +13,7 @@
 
 <style lang="postcss">
   .intro {
-    @apply text-teal-500 italic;
+    @apply text-red-500 italic font-extrabold text-4xl tracking-wider leading-loose;
   }
 </style>
 
@@ -27,17 +27,17 @@
 <main
   class="px-4 mx-auto mt-10 max-w-screen-xl sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
   <div class="text-center">
-    <h2
-        class="text-4xl font-extrabold tracking-tight text-gray-900 leading-10 sm:text-5xl sm:leading-none md:text-6xl">
-      {introduction}
-      <br class="xl:hidden" />
-      <span class="text-red-600">{app.title}</span>
+    <h2 class="flex items-center justify-center mb-10 text-4xl font-extrabold tracking-tight">
+        <span class="pr-3 text-yellow-500 rounded-r-full bg-gradient-to-bl to-gray-100 from-green-300">{introduction}</span>
     </h2>
+    <div class="flex items-center justify-center h-24 rounded-lg">
+        <span class="pr-2 text-4xl font-bold tracking-tight text-center rounded-l-full text-yellow-50 bg-gradient-to-l from-green-400 to-blue-400">{app.title}</span>
+      </div>
     <div
         class="max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
-      <h1 class="p-4 m-4 border rounded border-current">
+        <div class="p-4 m-4 border border-none rounded-full bg-gradient-to-r from-blue-50 to-cool-gray-200">
         <span class="intro">Elegant UI Using TailwindCSS</span>
-      </h1>
+      </div>
     </div>
   </div>
 </main>
